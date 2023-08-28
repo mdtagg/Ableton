7b8907f5838ae9b99681856ea5a9a546e0ca7938
Mike Tagg <mdtagg91@gmail.com>
2023-08-28 15:00:01 -0700

    styling added for nav elements

--- src/Navbar/index.tsx
+++ src/Navbar/index.tsx
@@ -4,20 +4,21 @@ const Navbar = () => {
     return (
 
     <nav>
-      <svg 
-        aria-hidden="true" 
-        focusable="false" 
-        className="nav-main-image"
-        xmlns="http://www.w3.org/2000/svg" 
-        width="45" 
-        height="21" 
-        viewBox="0 0 45 21"
-      >
-        <path 
-          d="M0 0h3v21H0zM6 0h3v21H6zM12 0h3v21h-3zM18 0h3v21h-3zM24 18h21v3H24zM24 12h21v3H24zM24 6h21v3H24zM24 0h21v3H24z">
-        </path>
-      </svg>
-      <ul className="nav-list">
+      
+    <ul className="nav-list">
+        <svg 
+            aria-hidden="true" 
+            focusable="false" 
+            className="nav-main-image"
+            xmlns="http://www.w3.org/2000/svg" 
+            width="45" 
+            height="21" 
+            viewBox="0 0 45 21"
+        >
+            <path 
+            d="M0 0h3v21H0zM6 0h3v21H6zM12 0h3v21h-3zM18 0h3v21h-3zM24 18h21v3H24zM24 12h21v3H24zM24 6h21v3H24zM24 0h21v3H24z">
+            </path>
+        </svg>
         <li><button>Live</button></li>
         <li><button>Push</button></li>
         <li><button>Note</button></li>
@@ -25,11 +26,16 @@ const Navbar = () => {
         <li><button>Shop</button></li>
         <li><button>Packs</button></li>
         <li><button>Help</button></li>
-        <li><button>More <span>+</span></button></li>
+        <li>
+            <button className="more-button">
+                <p>More</p>
+                <span>+</span>
+            </button>
+        </li>
       </ul>
       <ul className="nav-list trial-account">
-        <li><button>Try Live for Free</button></li>
-        <li>Log in or register</li>
+        <li><button id="trial-button">Try Live for Free</button></li>
+        <li><button className="log-in">Log in or register</button></li>
       </ul>
     </nav>
 
