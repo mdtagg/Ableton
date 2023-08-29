b865b07b979badeebb64d108ce40bd75da43b389
Mike Tagg <mdtagg91@gmail.com>
2023-08-29 11:21:58 -0700

    color background picture container added

--- src/App.tsx
+++ src/App.tsx
@@ -19,19 +19,26 @@ function App() {
           <p className="top-text">We make <a>Live</a>, <a>Push</a> and <a>Link</a> — unique software and hardware for music creation and performance. With these products, our community of users creates amazing things.</p>
           <p className="bottom-text">Ableton was founded in 1999 and released the first version of Live in 2001. Our products are used by a community of dedicated musicians, sound designers, and artists from across the world.</p>
         </section>
-        <div className="image-container">
-          <img
-            src="/src/assets/pexels-andrew-neel-2312369.jpg"
-            // height="130%"
-            width="50%"
-          ></img>
-          <img
-            src="/src/assets/pexels-proxyclick-visitor-management-system-2451567.jpg"
-            width="40%"
-          >
+        <div className="background-container">
+          
+          <div className="image-container">
+            <img
+              src="/src/assets/pexels-andrew-neel-2312369.jpg"
+              height="100%"
+              width="100%"
+            ></img>
+          </div>
 
-          </img>
+          <div className="color-image-container">
+            <img
+                src="/src/assets/pexels-proxyclick-visitor-management-system-2451567.jpg"
+                height="50%"
+                width="50%"
+              >
+              </img>
+          </div>
         </div>
+        
       </main>
     </>
   )
